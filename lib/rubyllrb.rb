111bd2e8e66cb0fb219dc054c706482faf8890cd
Alastair Gilles <agilles91@gmail.com>
2019-11-07 11:47:55 +0100

    Added basic binary tree behaviour
    The tree can now be searched and inserted into but it
    doesn't balance itself.
    Deletion also needs to be added

--- lib/rubyllrb.rb
+++ lib/rubyllrb.rb
@@ -1,4 +1,54 @@
 module RubyLLRB
+  RED = true
+  BLACK = false
+
   class LLRB
+    attr_reader :root_node, :size
+
+    def initialize
+      @size = 0
+    end
+
+    def insert(key, value)
+      @root_node = node_insert(@root_node, key, value)
+      @root_node.colour = BLACK
+    end
+
+    def search(key)
+      x = @root_node
+      until x.nil?
+        case key <=> x.key
+        when 0 then return x.value
+        when -1 then x = x.left
+        when 1 then x = x.right
+        end
+      end
+      return nil
+    end
+
+    private
+    def node_insert(node, key, value)
+      if node.nil?
+        @size += 1
+        return Node.new(key,value)
+      else
+        case key <=> node.key
+        when 0 then node.value = value
+        when -1 then node.left = node_insert(node.left, key, value)
+        when 1 then node.right = node_insert(node.right, key, value)
+        end
+        return node
+      end
+    end
+  end
+
+  class Node
+    attr_accessor :colour, :key, :value, :left, :right
+
+    def initialize(key, value)
+      @key = key
+      @value = value
+      @colour = RED
+    end
   end
 end
--- spec/rubyllrb_spec.rb
+++ spec/rubyllrb_spec.rb
@@ -1,9 +1,88 @@
 require 'rubyllrb'
 include RubyLLRB
 
+describe "constants" do
+  it "should have declared a RED constant" do
+    expect(RED).to eq true
+  end
+
+  it "should have declared a BLACK constant" do
+    expect(BLACK).to eq false
+  end
+end
+
 describe LLRB do
   let(:llrb) { LLRB.new }
   subject { llrb }
 
   it { is_expected.to be_instance_of LLRB }
+
+  it { is_expected.to have_attributes(root_node: nil) }
+  it { is_expected.to have_attributes(size: 0) }
+
+  it { is_expected.to respond_to(:insert).with(2).arguments }
+  it { is_expected.to respond_to(:search).with(1).arguments }
+
+  describe "#insert" do
+    it "should accept a key and value" do
+      expect{ subject.insert(1, "hello") }.to_not raise_error
+    end
+
+    it "should increase the size if a new key is added" do
+      subject.insert(1, :one)
+      expect(subject.size).to eq 1
+      subject.insert(2, :two)
+      expect(subject.size).to eq 2
+    end
+
+    it "shouldn't increase the size if the key already exists" do
+      subject.insert(1, :one)
+      subject.insert(1, :two)
+      expect(subject.size).to eq 1
+    end
+
+    it "should set the root to black" do
+      subject.insert(1, :one)
+      expect(subject.root_node.colour).to eq BLACK
+    end
+
+    it "should balance the tree (simple)" do
+      pending
+      subject.insert(1, :one)
+      subject.insert(2, :two)
+      subject.insert(3, :three)
+      expect(subject.root_node.key).to eq 2
+    end
+  end
+
+  describe "#search" do
+    it "should return a value when given a key" do
+      subject.insert(1, :one)
+      expect(subject.search(1)).to eq :one
+    end
+
+    it "should return nil when the tree is empty" do
+      expect(subject.search(1)).to be_nil
+    end
+
+    it "should return nil when the tree doesn't contain a key" do
+      subject.insert(1, :one)
+      subject.insert(2, :two)
+      subject.insert(3, :three)
+      expect(subject.search(4)).to be_nil
+    end
+  end
+end
+
+describe Node do
+  let(:node) { Node.new(:key, :value) }
+  subject { node }
+
+  it { is_expected.to be_instance_of Node }
+
+  it { is_expected.to have_attributes(colour: true) }
+  it { is_expected.to have_attributes(key: :key) }
+  it { is_expected.to have_attributes(value: :value) }
+  it { is_expected.to have_attributes(left: nil) }
+  it { is_expected.to have_attributes(right: nil) }
 end
