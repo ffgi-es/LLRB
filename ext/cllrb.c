3d9538a325b783071faf09b5123f1572b5695274
Alastair Gilles <agilles91@gmail.com>
2020-02-24 13:04:49 +0100

    refactor: change function parameters to be more specific

--- ext/cllrb.c
+++ ext/cllrb.c
@@ -1,12 +1,12 @@
 #include "ruby.h"
 #include "extconf.h"
 
-VALUE squareBrackets(int argc, VALUE* argv, VALUE obj) {
+VALUE squareBrackets(VALUE obj, VALUE index) {
     return rb_iv_get(obj, "@value");
 }
 
-VALUE assignSquareBrackets(int argc, VALUE* argv, VALUE obj) {
-    return rb_iv_set(obj, "@value", argv[1]);
+VALUE assignSquareBrackets(VALUE obj, VALUE index, VALUE value) {
+    return rb_iv_set(obj, "@value", value);
 }
 
 VALUE size(VALUE obj) {
@@ -16,7 +16,7 @@ VALUE size(VALUE obj) {
 void Init_cllrb() {
     VALUE rb_mCLLRB = rb_define_module("CLLRB");
     VALUE rb_cLLRB = rb_define_class_under(rb_mCLLRB, "LLRB", rb_cObject);
-    rb_define_method(rb_cLLRB, "[]", &squareBrackets, -1);
-    rb_define_method(rb_cLLRB, "[]=", &assignSquareBrackets, -1);
+    rb_define_method(rb_cLLRB, "[]", &squareBrackets, 1);
+    rb_define_method(rb_cLLRB, "[]=", &assignSquareBrackets, 2);
     rb_define_method(rb_cLLRB, "size", &size, 0);
 }
