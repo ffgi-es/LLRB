07422f8c3040a342c98821a97062405f2d803bfa
Alastair Gilles <agilles91@gmail.com>
2020-02-24 12:14:34 +0100

    feat: add [] method

--- ext/cllrb.c
+++ ext/cllrb.c
@@ -1,7 +1,12 @@
 #include "ruby.h"
 #include "extconf.h"
 
+VALUE squareBrackets(int argc, VALUE* argv, VALUE obj) {
+    return Qnil;
+}
+
 void Init_cllrb() {
     VALUE rb_mCLLRB = rb_define_module("CLLRB");
-    rb_define_class_under(rb_mCLLRB, "LLRB", rb_cObject);
+    VALUE rb_cLLRB = rb_define_class_under(rb_mCLLRB, "LLRB", rb_cObject);
+    rb_define_method(rb_cLLRB, "[]", &squareBrackets, -1);
 }
--- spec/cllrb_spec.rb
+++ spec/cllrb_spec.rb
@@ -1,4 +1,11 @@
 require '../ext/cllrb'
 
 describe CLLRB::LLRB do
+  subject { CLLRB::LLRB.new }
+
+  describe "#[]" do
+    it "should return nil for an empty tree" do
+      expect(subject[1]).to be_nil
+    end
+  end
 end
